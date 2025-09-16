2718ed73c2bb6fe111eab4f5e06f9e298ee38953
Cherik <pourcheriki@gmail.com>
2025-09-17 00:47:32 +0330

    show user data in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import { InfoList } from "@/components/InfoList";
 import { ActionButtonList } from "@/components/ActionButtonList";
 import Image from 'next/image';
 import ClientGate from '@/components/ClientGate'
+import { useTelegramUser } from '@/hooks/useTelegramUser'
 
 export default async function Home() {
   // Simulate work to show the global loading UI
-  await new Promise((r) => setTimeout(r, 1500));
+  // await new Promise((r) => setTimeout(r, 1500));
+  const { user } = useTelegramUser()
+
 
   return (
     <ClientGate>
       <div className={"pages"}>
         <Image src="/reown.svg" alt="Reown" width={150} height={150} priority />
         <h1>AppKit Wagmi Next.js App Router Example</h1>
-
+        user: {user?.first_name} {user?.last_name} {user?.username} {user?.language_code}
         <ConnectButton />
         <ActionButtonList />
         <div className="advice">
