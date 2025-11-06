918c9df28b801da5e1343097d4c648023882f19b
Cherik <pourcheriki@gmail.com>
2025-11-06 17:22:13 +0330

    add wheel page title

---
+++ src/app/wheel/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WheelPage = () => {
+  return <div className='rounded-2xl border-2 border-base-300 px-4 py-6'>
+    <div className='text-center font-kalame font-black'>
+        <div className='text-5xl'>گردونه رو</div>
+        <div className='text-8xl text-primary -mt-4'>بچرخون</div>
+    </div>
+  </div>;
+};
+
+export default WheelPage;
